aaba83cb8e6256f815ce646ff79aebc2a298c79c
Martin <martin.ambronn@outlook.de>
2025-05-24 12:39:11 +0200

    Menü Update

--- Scripts/start_menu_scene.gd
+++ Scripts/start_menu_scene.gd
@@ -2,4 +2,8 @@ extends Node2D
 
 
 func _on_button_start_pressed():
+	
 	SceneTransition.change_scene("res://Scenes/elevator.tscn")
+	$ParallaxAtHome/Doors.hide()
+	$AnimationLogo.play("LogoDown")
+	
