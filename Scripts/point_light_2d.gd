087f3706384774c3598cb86797ac2e3faa9567f3
K <sophie.s.1@outlook.de>
2025-05-25 12:42:32 +0200

    Light Flickers & Updated Backgrounds

--- Scripts/point_light_2d.gd
+++ Scripts/point_light_2d.gd
@@ -1,7 +1,20 @@
 extends PointLight2D
 
+# Timer für Lichteffekte
+@onready var flicker_timer: Timer = $"../FlickerTimer"
+@onready var flicker_start_timer: Timer = $"../FlickerStartTimer"
+
+# Einstellung für Lag
 var follow_speed := 8.0
 var initialized := false
+
+var flicker_duration := 0.8
+var flicker_elapsed := 0.0
+
+func _ready():
+	randomize()
+	energy = 1.0 # Wert für volles Licht
+	start_flicker_delay()
 
 func _physics_process(delta):
 	var target = get_global_mouse_position()
@@ -12,3 +25,22 @@ func _physics_process(delta):
 		initialized = true
 	else:
 		position = position.lerp(local_target, delta * follow_speed)
+
+func start_flicker_delay():
+	var wait_time = randf_range(4.0, 10.0)  # 4 bis 10 Sekunden bis nächstes Flackern
+	flicker_start_timer.start(wait_time)
+
+func _on_flicker_start_timer_timeout():
+	flicker_elapsed = 0.0
+	flicker_timer.start(0.05)  # Flackern jede 50ms
+
+func _on_flicker_timer_timeout():
+	flicker_elapsed += flicker_timer.wait_time
+	if flicker_elapsed >= flicker_duration:
+		# Ende
+		energy = 1.0
+		flicker_timer.stop()
+		start_flicker_delay()
+	else:
+		# Flackern
+		energy = randf_range(0.3, 1.0)
